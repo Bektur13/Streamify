94c6be61d570ff82c3e83fe1d018fb1832595335
Bektur13 <duyshembekov04@mail.ru>
2025-04-29 11:45:01 -0700

    Installed GraphQL & Config dependencies, GraphQL & Redis Setup, Added utility functions

---
+++ server/src/core/redis/redis.service.ts
@@ -0,0 +1,21 @@
+// import { Injectable } from '@nestjs/common'
+// import { ConfigService } from '@nestjs/config'
+// import Redis from 'ioredis'
+
+// @Injectable()
+// export class RedisService extends Redis {
+//   constructor(private readonly configService: ConfigService) {
+//     super(configService.getOrThrow<string>('REDIS_URI'))
+//   }
+// }
+
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService extends Redis {
+    constructor(private readonly configService: ConfigService) {
+        super(configService.getOrThrow<string>('REDIS_URL'))
+    }
+}
