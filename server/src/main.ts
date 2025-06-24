7f4a9ae6039c9dec923f7a9450b58c398e14d8f0
Bektur13 <duyshembekov04@mail.ru>
2025-06-24 14:09:20 -0700

    Updated main.ts and have to fix errors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,64 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CoreMo}
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+import { RedisStore } from 'connect-redis';
+import { CoreModule } from './core/core.module';
+
+import { RedisService } from './core/redis/redis.service';
+import { ms, StringValue } from './shared/utils/ms.util';
+
+import { parseBoolean } from './shared/utils/parse-boolean.util';
+import { log } from 'console';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(CoreModule)
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(CoreModule, { rawBody: true})
+
+  const config = app.get(ConfigService);
+  
+  const redis = app.get(RedisService);
+
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
   await app.listen(process.env.PORT ?? 3000);
+  app.use(config.getOrThrow<string>('GRPAHQL_PREFIX'), graphqlUploadExpress())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true
+    })
+  )
+
+  app.use(
+    session({
+      secret: config.getOrThrow<string>('SESSION_SECRET'), 
+      name: config.getOrThrow<string>('SESSION_NAME'),
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        domain: config.getOrThrow<string>('SESSION_DOMIAN'),
+        maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+        httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+        secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+        sameSite: 'lax'
+      },
+      store: new RedisStore({
+        client: redis,
+        prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+        ttl: ms(config.getOrThrow<StringValue>('REDIS_TTL'))
+      })
+    })
+  )
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie']
+  })
+
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'))
+  console.log(`✅ Server is running at: ${config.getOrThrow<string>('APPLICATION_URL')}`)
 }
 bootstrap();
