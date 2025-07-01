687f64b2ced9af5917e0a68e9ab0a754d18d6380
Bektur13 <duyshembekov04@mail.ru>
2025-07-01 11:29:06 -0700

    Fixed my Schema.prisma

--- server/src/modules/auth/account/account.resolver.ts
+++ server/src/modules/auth/account/account.resolver.ts
@@ -1,13 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
-import { AccountService } from './account.service';
-import { UserModel } from './models/user.model'
+// import { Query, Resolver } from '@nestjs/graphql';
+// import { AccountService } from './account.service';
+// import { UserModel } from './models/user.model'
 
-@Resolver('Account')
-export class AccountResolver {
-  constructor(private readonly accountService: AccountService) {}
+// @Resolver('Account')
+// export class AccountResolver {
+//   constructor(private readonly accountService: AccountService) {}
 
-  @Query(() => [UserModel], { name: 'findAllUsers'})
-  public async findAll() {
-    return this.accountService.findAll()
-  }
-}
+//   @Query(() => [UserModel], { name: 'findAllUsers' })
+//   public async findAll() {
+//     return this.accountService.findAll()
+//   }
+
+// }
--- server/src/modules/auth/account/models/user.model.ts
+++ server/src/modules/auth/account/models/user.model.ts
@@ -1,32 +1,32 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql'
-import { User } from '@prisma/client'
+// import { Field, ID, ObjectType } from '@nestjs/graphql'
+// import { Prisma } from '../../../../'
 
-@ObjectType()
-export class UserModel implements User {
-  @Field(() => ID)
-  public id: string
+// @ObjectType()
+// export class UserModel implements User {
+//   @Field(() => ID)
+//   public id: string
 
-  @Field(() => String, { nullable: true })
-  public email: string
+//   @Field(() => String, { nullable: true })
+//   public email: string
 
-  @Field(() => String)
-  public password: string
+//   @Field(() => String)
+//   public password: string
 
-  @Field(() => String)
-  public username: string
+//   @Field(() => String)
+//   public username: string
 
-  @Field(() => String)
-  public displayName: string
+//   @Field(() => String)
+//   public displayName: string
 
-  @Field(() => String, { nullable: true })
-  public avatar: string
+//   @Field(() => String, { nullable: true })
+//   public avatar: string
 
-  @Field(() => String, { nullable: true })
-  public bio: string
+//   @Field(() => String, { nullable: true })
+//   public bio: string
 
-  @Field(() => Date)
-  public createdAt: Date
+//   @Field(() => Date)
+//   public createdAt: Date
 
-  @Field(() => Date)
-  public updatedAt: Date
-}
+//   @Field(() => Date)
+//   public updatedAt: Date
+// }
