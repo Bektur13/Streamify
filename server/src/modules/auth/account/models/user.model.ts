a3cdc615d280ae79c806d7b33e5819e54bacbdf9
Bektur13 <duyshembekov04@mail.ru>
2025-06-30 18:30:45 -0700

    Created the user GraphQL model

--- server/src/modules/auth/account/models/user.model.ts
+++ server/src/modules/auth/account/models/user.model.ts
@@ -10,5 +10,24 @@ export class UserModel implements User {
     public email: string
 
     @Field(() => String)
-    public password: 
+    public password: string
+
+    @Field(() => String) 
+    public username: string
+
+    @Field(() => String) 
+    public displayName: string
+
+    @Field(() => String, { nullable: true }) 
+    public avatar: string
+
+    @Field(() => String, { nullable: true })
+    public bio: string
+    
+    @Field(() => Date)
+    public createdAt: Date
+
+    @Field(() => Date)
+    public updatedAt: Date
+
 }
