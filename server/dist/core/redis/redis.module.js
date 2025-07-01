ce72bfb619f64b9677d7f2d7393786d35dd79e26
Bektur13 <duyshembekov04@mail.ru>
2025-07-01 12:04:24 -0700

    Started fixing Redis errors

--- server/dist/core/redis/redis.module.js
+++ server/dist/core/redis/redis.module.js
@@ -10,14 +10,17 @@ exports.RedisModule = void 0;
 const common_1 = require("@nestjs/common");
 const redis_service_1 = require("./redis.service");
 const redis_controller_1 = require("./redis.controller");
+const config_1 = require("@nestjs/config");
 let RedisModule = class RedisModule {
 };
 exports.RedisModule = RedisModule;
 exports.RedisModule = RedisModule = __decorate([
     (0, common_1.Global)(),
     (0, common_1.Module)({
+        imports: [config_1.ConfigModule],
         controllers: [redis_controller_1.RedisController],
         providers: [redis_service_1.RedisService],
+        exports: [redis_service_1.RedisService]
     })
 ], RedisModule);
 //# sourceMappingURL=redis.module.js.map
--- server/src/core/redis/redis.module.ts
+++ server/src/core/redis/redis.module.ts
@@ -1,10 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 import { RedisController } from './redis.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   controllers: [RedisController],
   providers: [RedisService],
+  exports: [RedisService]
 })
 export class RedisModule {}
