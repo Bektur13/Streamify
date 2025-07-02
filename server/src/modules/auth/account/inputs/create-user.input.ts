e4e4b8732eccbac559da8205413da9e017195ef9
Bektur13 <duyshembekov04@mail.ru>
2025-07-02 13:01:06 -0700

    Added creating user input

---
+++ server/src/modules/auth/account/inputs/create-user.input.ts
@@ -0,0 +1,26 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
+import { stringify } from "querystring";
+
+@InputType()
+export class CreateUserInput {
+    @Field(() => String)
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/)
+    public username: string
+    
+    @Field(() => String)
+    @IsEmail()
+    @IsNotEmpty()
+    public email: string
+
+    @Field(() => String)
+    @IsNotEmpty()
+    @MinLength(8)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+    message:
+      'Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character',
+    })
+    public password: string
+}
