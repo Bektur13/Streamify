8a37aaee8251689c9a0b012b4cf71e387bf9c646
Bektur13 <duyshembekov04@mail.ru>
2025-07-07 16:43:10 -0700

    Generated session module and added login input dto

---
+++ server/src/modules/auth/account/inputs/login.input.ts
@@ -0,0 +1,21 @@
+import { Field, InputType } from '@nestjs/graphql'
+import { 
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Length,
+    MinLength
+} from 'class-validator'
+
+@InputType() 
+export class LoginInput {
+    @Field()
+    @IsString()
+    @IsNotEmpty()
+    public login: string
+
+    @Field()
+    @IsString()
+    @MinLength(8)
+    public password: string
+}
