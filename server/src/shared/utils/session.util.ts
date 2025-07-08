84aab0a5e5e5ee50e77ae0439bddc6658bf2764a
Bektur13 <duyshembekov04@mail.ru>
2025-07-07 17:01:25 -0700

    Created session utilities but have errors need to fix them

---
+++ server/src/shared/utils/session.util.ts
@@ -0,0 +1,46 @@
+import { InternalServerErrorException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { Request } from "@nestjs/common";
+
+import type { User } from "../../../generated/prisma";
+
+export function saveSession(
+    req: Request
+    user: User,
+) {
+    return new Promise((resolve, reject) => {
+        req.session.createdAt = new Date()
+        req.session.userId = user.id
+
+        req.session.save(err => {
+            if(err) {
+                return reject(
+                    new InternalServerErrorException(
+                        'Не удалось сохранить сессию'
+                    )
+                )
+            }
+
+            resolve(user)
+        })
+    }) 
+}
+
+
+export function destroySession(req: Request, ConfigService: ConfigService) {
+    return new Promise((resolve, reject) => {
+        req.session.destroy(err => {
+            if(err) {
+                return new InternalServerErrorException(
+                    'Не удалось завершить сессию'
+                )
+            }
+
+            req.res.clearCookie(
+                ConfigService.getOrThrow<string>('SESSION_NAME')
+            )
+
+            resolve(true)
+        })
+    })
+}
